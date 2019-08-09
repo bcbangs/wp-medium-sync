96f2094088425814b599cca903c3a9425ade7837
Fabian Böttcher <fabian.boettcher@bcbangs.com>
2019-08-09 17:08:36 +0200

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name: Medium Sync
+ * Version: 1.0.0
+ * Plugin URI: https://www.bcbangs.com
+ * Description: A WordPress Plugin to synchronize your Medium posts to WordPress.
+ * Author: Fabian Böttcher
+ * Author URI: https://www.bcbangs.com/team
+ * Requires at least: 5.2
+ * Tested up to: 5.2
+ *
+ * @author Fabian Böttcher <fabian.boettcher@bcbangs.com>
+ * @since 1.0.0
+ */
